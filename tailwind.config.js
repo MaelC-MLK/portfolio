533220e4a1481b0942daad9f664fe85822fb93b2
Mael C <74120418+MaelC-MLK@users.noreply.github.com>
2024-01-02 12:59:12 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'blue-dark':'var(--primary-color)',
+        'blue-light':'var(--third-color)',
+        'beige':'var(--secondary-color)',
+        'grey':'var(--fourth-color)',
+        'navbar':'var(--navbar-color)',
+      },
+      fontFamily: {
+        'inria':['Inria Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
